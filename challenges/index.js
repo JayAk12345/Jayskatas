a362b03a8651ca9e45fa5c1fce0b836c71d2e049
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-06-30 21:07:38 +0100

    formatMoney refactored

--- challenges/index.js
+++ challenges/index.js
@@ -26,18 +26,8 @@ function formatMoney (amount) {
    * Amounts should always have 2 decimal numbers even if they are zeroes
    * formatMoney(1) => "£1.00"
    */
-   console.log(typeof amount);
-   let answer = amount.toFixed(2);
-   console.log(typeof answer, "ANSWER");
-
-let string = "£" + answer;
-console.log(string);
-
- //  let string = answer.toString();
- //  console.log(typeof string, "STRING");
-   
-
-return string
+   let string = "£" + amount.toFixed(2); 
+   return string
 }
 
 
